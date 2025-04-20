024945893870a8b4f79ca6275e1bc7f13c6be3ad
alfajauhari068 <148884542+alfajauhari068@users.noreply.github.com>
2025-04-20 15:07:53 +0700

    Update pengiriman.py

--- pengiriman.py
+++ pengiriman.py
@@ -1,6 +1,6 @@
-# Misal: jalan dengan bobot lalu lintas
+
 jalan = {
-    "A": {"B": 2, "E": 1},  # Bobot 2 dari A ke B
+    "A": {"B": 2, "E": 1},  
     "B": {"A": 2, "C": 3},
-    # ... dst
+    
 }
